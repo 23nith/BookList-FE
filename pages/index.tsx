17ef2a147f1f1b1d278e9f279dbbbd4f93998654
23nith <zenithmanuel@gmail.com>
2022-08-24 15:24:59 +0800

    Add forms

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,43 @@ import styles from '../styles/Home.module.css'
 import {Dialog} from '@reach/dialog'
 import { useState } from 'react'
 
+function LoginForm({onSubmit, buttonText}){
+  function handleSubmit(event: React.FormEvent<HTMLInputElement>){
+    event.preventDefault()
+    const {username, password} = event.target.elements
+
+    onSubmit({
+      username: username.value,
+      password: password.value
+    })
+  }
+  return(
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input id="password" type="text" />
+      </div>
+      <div>
+        <button type="submit">{buttonText}</button>
+      </div>
+    </form>
+  )
+}
+
 const Home: NextPage = () => {
   const [openModal, setOpenModal] = useState('none')
+
+  function login(formData: React.FormEvent<HTMLInputElement>){
+    console.log('login', formData)
+  }
+
+  function register(formData: React.FormEvent<HTMLInputElement>){
+    console.log('register', formData)
+  }
 
   return (
     <div className={`${styles.container} ${styles.homeContainer}`}>
@@ -20,12 +55,14 @@ const Home: NextPage = () => {
           <button onClick={()=>{ setOpenModal('none')}}>Close</button>
         </div>
         <h3>Login</h3>
+        <LoginForm onSubmit={login} buttonText="Login"/>
       </Dialog>
       <Dialog aria-label="Login form" isOpen={openModal === 'register'}>
         <div>
           <button onClick={()=>{ setOpenModal('none')}}>Close</button>
         </div>
         <h3>Register</h3>
+        <LoginForm onSubmit={register} buttonText="Register"/>
       </Dialog>
       
     </div>
