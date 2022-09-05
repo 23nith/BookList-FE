b46ad615cac1227f003ed9d5c712de4d0e0627ce
23nith <zenithmanuel@gmail.com>
2022-09-06 02:09:04 +0800

    Create finished books list context and render logic

---
+++ contexts/FinishedBooksContext.tsx
@@ -0,0 +1,33 @@
+import React, { createContext, useState } from "react";
+
+export const FinishedBooks = createContext();
+
+function FinishedBooksProvider(props) {
+  const [finishedBooks, setFinishedBooks] = useState([]);
+  const fetchFinishedBooks = () => {
+    fetch("http://localhost:3000/api/v1/finished_books", {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("token"),
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setFinishedBooks(data);
+        return data;
+      });
+  };
+
+  return (
+    <FinishedBooks.Provider
+      value={{ finishedBooks, setFinishedBooks, fetchFinishedBooks }}
+    >
+      {props.children}
+    </FinishedBooks.Provider>
+  );
+}
+
+export default FinishedBooksProvider;
--- pages/finished.tsx
+++ pages/finished.tsx
@@ -1,19 +1,40 @@
 import type { NextPage } from "next";
 import Link from "next/link";
+import { useContext, useEffect, useState } from "react";
+import { FinishedBooks } from "../contexts/FinishedBooksContext";
+import BookList from "../components/BookList";
 
 const finished: NextPage = () => {
+  const { finishedBooks, setFinishedBooks, fetchFinishedBooks } =
+    useContext(FinishedBooks);
+
+  const [Finished, setFinished] = useState(() => {
+    if (finishedBooks.length == 0) {
+      console.log("blank");
+      fetchFinishedBooks();
+    }
+  });
+
+  useEffect(() => {
+    console.log("Finished books: ", finishedBooks);
+  }, [finishedBooks]);
+
   return (
     <>
-      <div className="text-center text-xl">
-        <p className="my-6">
-          Hey there! This is where books will go when you've finished reading
-          them. Get started by heading over to the{" "}
-          <Link href="/discover">
-            <a className="text-blue-500">Discover</a>
-          </Link>{" "}
-          page to add books to your list.
-        </p>
-      </div>
+      {!finishedBooks && (
+        <div className="text-center text-xl">
+          <p className="my-6">
+            Hey there! This is where books will go when you've finished reading
+            them. Get started by heading over to the{" "}
+            <Link href="/discover">
+              <a className="text-blue-500">Discover</a>
+            </Link>{" "}
+            page to add books to your list.
+          </p>
+        </div>
+      )}
+      {finishedBooks &&
+        finishedBooks.map((listItem) => <BookList book={listItem.book} />)}
     </>
   );
 };
