0578a4e25f6ad543c49c073b57b143004b7a9d42
23nith <zenithmanuel@gmail.com>
2022-09-07 03:00:34 +0800

    Create modal component

---
+++ components/Modal.tsx
@@ -0,0 +1,46 @@
+import React, {
+  cloneElement,
+  createContext,
+  useContext,
+  useState,
+} from "react";
+import { Dialog } from "@reach/dialog";
+import styles from "../styles/Home.module.css";
+
+const ModalContext = createContext();
+
+const Modal = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return <ModalContext.Provider value={[isOpen, setIsOpen]} {...props} />;
+};
+
+const ModalDismissButton = ({ children: child }) => {
+  const [, setIsOpen] = useContext(ModalContext);
+  return cloneElement(child, {
+    onClick: () => setIsOpen(false),
+  });
+};
+
+const ModalOpenButton = ({ children: child }) => {
+  const [, setIsOpen] = useContext(ModalContext);
+  return cloneElement(child, {
+    onClick: () => setIsOpen(true),
+  });
+};
+
+const ModalContents = (props) => {
+  const [isOpen, setIsOpen] = useContext(ModalContext);
+  return (
+    <Dialog
+      isOpen={isOpen}
+      onDismiss={() => {
+        setIsOpen(false);
+      }}
+      className={styles.formContainer}
+      {...props}
+    />
+  );
+};
+
+export { Modal, ModalDismissButton, ModalOpenButton, ModalContents };
