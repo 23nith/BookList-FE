172141deedb2819604a4bc48daba5e2a34f197d5
23nith <zenithmanuel@gmail.com>
2022-09-07 03:09:52 +0800

    Use modal component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,24 @@ import "@reach/dialog/styles.css";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 import { Dialog } from "@reach/dialog";
-import { useState } from "react";
+import { cloneElement, useState } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import Head from "next/head";
+import tw from "tailwind-styled-components";
+import {
+  Modal,
+  ModalDismissButton,
+  ModalContents,
+  ModalOpenButton,
+} from "../components/Modal";
 
-const LoginForm = ({ onSubmit, buttonText }) => {
+const Button = tw.button`py-10px px-15px border-0 leading-none rounded-3`;
+
+const LoginButton = tw(Button)`text-white bg-custom_blue`;
+const RegisterButton = tw(Button)`text-custom_gray bg-custom_light_gray2`;
+
+const LoginForm = ({ onSubmit, submitButton }) => {
   const handleSubmit = (event: React.FormEvent<HTMLInputElement>) => {
     event.preventDefault();
     const { username, password } = event.target.elements;
@@ -28,20 +40,28 @@ const LoginForm = ({ onSubmit, buttonText }) => {
         <input id="password" type="password" className="dialogInputField" />
       </div>
       <div className="loginFormButtonDiv">
-        <button
-          className={
-            buttonText == "Login"
-              ? "loginFormButtonLogin"
-              : "loginFormButtonRegister"
-          }
-          type="submit"
-        >
-          {buttonText}
-        </button>
+        {cloneElement(
+          submitButton,
+          { type: "submit" },
+          ...(Array.isArray(submitButton.props.children)
+            ? submitButton.props.children
+            : [submitButton.props.children])
+        )}
       </div>
     </form>
   );
 };
+
+const circleDismissButton = (
+  <div className="closeFormDiv">
+    <ModalDismissButton>
+      <button className="closeFormButton">
+        <span className="closeFormSpan">Close</span>
+        <span aria-hidden="true">x</span>
+      </button>
+    </ModalDismissButton>
+  </div>
+);
 
 const Home: NextPage = () => {
   const [openModal, setOpenModal] = useState<String>("none");
@@ -105,61 +125,34 @@ const Home: NextPage = () => {
         <Image src="/logo.png" width={80} height={80} />
         <h1 className={styles.bookshelfH1}>Bookshelf</h1>
         <div className={styles.buttonContainer}>
-          <button
-            className="loginFormButtonLogin"
-            onClick={() => {
-              setOpenModal("login");
-            }}
-          >
-            Login
-          </button>
-          <button
-            className="loginFormButtonRegister"
-            onClick={() => {
-              setOpenModal("register");
-            }}
-          >
-            Register
-          </button>
+          <Modal>
+            <ModalOpenButton>
+              <LoginButton>Login</LoginButton>
+            </ModalOpenButton>
+            <ModalContents aria-label="">
+              {circleDismissButton}
+              <h3 className="dialogH3">Login</h3>
+              <LoginForm
+                onSubmit={login}
+                submitButton={<LoginButton>Login</LoginButton>}
+              />
+            </ModalContents>
+          </Modal>
+
+          <Modal>
+            <ModalOpenButton>
+              <RegisterButton>Register</RegisterButton>
+            </ModalOpenButton>
+            <ModalContents aria-label="">
+              {circleDismissButton}
+              <h3 className="dialogH3">Register</h3>
+              <LoginForm
+                onSubmit={register}
+                submitButton={<RegisterButton>Register</RegisterButton>}
+              />
+            </ModalContents>
+          </Modal>
         </div>
-        <Dialog
-          aria-label="Login form"
-          isOpen={openModal === "login"}
-          className={styles.formContainer}
-        >
-          <div className="closeFormDiv">
-            <button
-              onClick={() => {
-                setOpenModal("none");
-              }}
-              className="closeFormButton"
-            >
-              <span className="closeFormSpan">Close</span>
-              <span aria-hidden="true">x</span>
-            </button>
-          </div>
-          <h3 className="dialogH3">Login</h3>
-          <LoginForm onSubmit={login} buttonText="Login" />
-        </Dialog>
-        <Dialog
-          aria-label="Registration form"
-          isOpen={openModal === "register"}
-          className={styles.formContainer}
-        >
-          <div className="closeFormDiv">
-            <button
-              onClick={() => {
-                setOpenModal("none");
-              }}
-              className="closeFormButton"
-            >
-              <span className="closeFormSpan">Close</span>
-              <span aria-hidden="true">x</span>
-            </button>
-          </div>
-          <h3 className="dialogH3">Register</h3>
-          <LoginForm onSubmit={register} buttonText="Register" />
-        </Dialog>
       </div>
     </>
   );
