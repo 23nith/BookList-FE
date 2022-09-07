3f37e39c9149ca4877d41c2818086c46841d576c
23nith <zenithmanuel@gmail.com>
2022-09-07 15:08:36 +0800

    Convert modal context values from array into object

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -12,25 +12,25 @@ const ModalContext = createContext();
 const Modal = (props) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  return <ModalContext.Provider value={[isOpen, setIsOpen]} {...props} />;
+  return <ModalContext.Provider value={{ isOpen, setIsOpen }} {...props} />;
 };
 
 const ModalDismissButton = ({ children: child }) => {
-  const [, setIsOpen] = useContext(ModalContext);
+  const { setIsOpen } = useContext(ModalContext);
   return cloneElement(child, {
     onClick: () => setIsOpen(false),
   });
 };
 
 const ModalOpenButton = ({ children: child }) => {
-  const [, setIsOpen] = useContext(ModalContext);
+  const { setIsOpen } = useContext(ModalContext);
   return cloneElement(child, {
     onClick: () => setIsOpen(true),
   });
 };
 
 const ModalContents = (props) => {
-  const [isOpen, setIsOpen] = useContext(ModalContext);
+  const { isOpen, setIsOpen } = useContext(ModalContext);
   return (
     <Dialog
       isOpen={isOpen}
