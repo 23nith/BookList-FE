a80214c0bd2aa5d0c7c8e700721315b5be903f70
23nith <zenithmanuel@gmail.com>
2022-09-08 21:48:34 +0800

    Fix style of navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,24 @@
 import Link from "next/link";
 import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
 
 const Navbar: NextPage = () => {
   return (
-    <div className="grow border-2 border-blue border-solid h-full bg-blue-200 p-1 min-w-200">
+    <div className="grow h-full  p-1 min-w-200">
       <div className="pt-4">
-        <ul className="border-black border-2 border-solid p-4 ">
-          <li className="border-black border-2 border-solid pl-2 py-3">
+        <ul className={styles.nav_box}>
+          <li className={styles.nav_li_active}>
             <Link href="/list">
               <a>Reading List</a>
             </Link>
           </li>
-          <li className="border-black border-2 border-solid pl-2 py-3">
+          {/* <li className="border-black border-2 border-solid pl-2 py-3"> */}
+          <li className="pl-2 py-3">
             <Link href="/finished">
               <a>Finished Books</a>
             </Link>
           </li>
-          <li className="border-black border-2 border-solid pl-2 py-3">
+          <li className="pl-2 py-3">
             <Link href="/discover">
               <a>Discover</a>
             </Link>
