1c333a8ddc871d35d92ec17c06564a408d19701e
23nith <zenithmanuel@gmail.com>
2022-09-14 14:00:30 +0800

    Cherry pick from create_finished_books 920ad68 to dry up api function

---
+++ api/finished_books.ts
@@ -0,0 +1,26 @@
+import { IListItem } from "../Interfaces/interfaces";
+import { authHeaders, baseUrl } from "./base";
+
+export interface IFinishedBooksProps {
+  setIsLoading: React.Dispatch<React.SetStateAction<Boolean>>;
+  setFinishedBooks: IListItem[];
+}
+
+export const fetchFinishedBooks = ({
+  setIsLoading,
+  setFinishedBooks,
+}: IFinishedBooksProps): Promise<IListItem[]> => {
+  setIsLoading(true);
+  fetch(`${baseUrl()}/api/v1/finished_books`, {
+    method: "get",
+    headers: { ...authHeaders() },
+  })
+    .then((res) => {
+      setIsLoading(false);
+      return res.json();
+    })
+    .then((data) => {
+      setFinishedBooks(data);
+      return data;
+    });
+};
