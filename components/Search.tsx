7d7b82bc116037208e5afe55e9dd2d9da581e66f
23nith <zenithmanuel@gmail.com>
2022-09-15 18:45:41 +0800

    Convert search component into formik

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,12 +1,26 @@
+import { useFormik } from "formik";
 import React from "react";
 
 function Search() {
+  const formik = useFormik({
+    initialValues: {
+      query: "",
+    },
+    onSubmit: (values) => {
+      console.log(values.query);
+    },
+  });
   return (
-    <input
-      type="text"
-      className="bg-custom_light_gray rounded-3 px-4 py-3 w-full text-custom_gray"
-      placeholder="Search books ..."
-    />
+    <form onSubmit={formik.handleSubmit}>
+      <input
+        id="query"
+        type="text"
+        className="bg-custom_light_gray rounded-3 px-4 py-3 w-full text-custom_gray"
+        placeholder="Search books ..."
+        onChange={formik.handleChange}
+        value={formik.values.query}
+      />
+    </form>
   );
 }
 
