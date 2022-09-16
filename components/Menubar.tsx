30c8bd131dbb561f64b2138646ec9fd5d1a88fa4
23nith <zenithmanuel@gmail.com>
2022-09-16 14:57:03 +0800

    Add styling to Menubar

--- components/Menubar.tsx
+++ components/Menubar.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from "next";
+import { LogoutButton } from "./styled";
 
 const Menubar: NextPage = () => {
   return (
     <div className="flex justify-end p-2">
-      <button></button>
-      <button>Logout</button>
+      <div className="py-10px px-15px leading-none">Luke</div>
+      <LogoutButton>Logout</LogoutButton>
     </div>
   );
 };
