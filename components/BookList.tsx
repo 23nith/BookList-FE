a9f8ec5a51a8a3d179af31b8d8f5759912434a21
23nith <zenithmanuel@gmail.com>
2022-09-16 23:33:22 +0800

    Render book on book page after click event

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -1,10 +1,12 @@
-import type { NextPage } from "next";
+import { useContext } from "react";
 import styles from "../styles/Home.module.css";
 import { MdAddCircle } from "react-icons/md";
 import { BsCheckCircleFill } from "react-icons/bs";
 import { FaMinusCircle } from "react-icons/fa";
 import { FaBook } from "react-icons/fa";
 import { IBook } from "../api/types";
+import { ShowBookContext } from "../contexts/ShowBookContext";
+import { useRouter } from "next/router";
 
 interface BooklistProps {
   book: IBook;
@@ -12,14 +14,17 @@ interface BooklistProps {
 }
 
 const BookList = ({ book, state }: BooklistProps) => {
+  const router = useRouter();
+  const { setBook } = useContext(ShowBookContext);
   const handleBookClick = (e) => {
-    console.log(e);
+    setBook(e);
+    router.push("/book");
   };
   return (
     <div
       className="booklist_box cursor-pointer"
       onClick={(e) => {
-        handleBookClick(book.id);
+        handleBookClick(book);
       }}
     >
       <div className="grow p-5">
--- pages/book.tsx
+++ pages/book.tsx
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShowBookContext } from "../contexts/ShowBookContext";
 
 function book() {
+  const { book } = useContext(ShowBookContext);
+
   return (
     <div className="border-2 border-black border-solid flex flex-row">
-      <div className="border-2 border-black border-solid grow">image</div>
+      <div className="border-2 border-black border-solid grow">
+        <img
+          src={book.cover_image_url}
+          width={140}
+          height={210}
+          className="min-w-140"
+        />
+      </div>
       <div className="border-2 border-black border-solid grow-3">
         <div className="flex flex-row">
           <div className="grow-[3] border-2 border-black border-solid">
-            <div>Title</div>
+            <div>{book.title}</div>
             <div className="flex flex-row border-2 border-black border-solid">
-              <div className="px-3">author</div> |{" "}
-              <div className="px-3">publisher</div>
+              <div className="px-3">{book.author}</div> |{" "}
+              <div className="px-3">{book.publisher}</div>
             </div>
           </div>
           <div className="grow-[1] border-2 border-black border-solid max-w-[24px]">
             <div className="max-w-[24px]">x</div>
           </div>
         </div>
-        <div>synopsis</div>
+        <div>{book.synopsis}</div>
       </div>
     </div>
   );
