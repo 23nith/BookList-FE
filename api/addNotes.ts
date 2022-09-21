6bc639826044004320779427d7423732049da7b2
23nith <zenithmanuel@gmail.com>
2022-09-21 18:42:55 +0800

    Add JSON.stringify to addNotes API

--- api/addNotes.ts
+++ api/addNotes.ts
@@ -2,6 +2,7 @@ import { authHeaders, baseUrl } from "./base";
 import { ListItem } from "./types";
 
 export const addNotes = (notes: string, list: ListItem) => {
+  console.log("notes: ", notes);
   fetch(`${baseUrl()}/api/v1/list_item/${list.id}`, {
     method: "PATCH",
     headers: { ...authHeaders() },
