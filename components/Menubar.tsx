35ebf4889537defbf344ceebf37b492733e26979
23nith <zenithmanuel@gmail.com>
2022-09-22 17:29:25 +0800

    Add handleLogout to Menubar component

--- components/Menubar.tsx
+++ components/Menubar.tsx
@@ -1,15 +1,23 @@
 import { LogoutButton } from "./styled";
 import { useContext, useEffect } from "react";
 import { UserContext } from "../contexts/UserContext";
-import { setCurrentUser } from "../api/setCurrentUser";
+import { logout } from "../api/logout";
+import { useRouter } from "next/router";
 
 const Menubar = () => {
-  const { user } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
+  const router = useRouter();
+
+  const handleLogout = () => {
+    logout();
+    setUser(null);
+    router.push("/");
+  };
 
   return (
     <div className="flex justify-end p-2">
       <div className="py-10px px-15px leading-none">{user.username}</div>
-      <LogoutButton>Logout</LogoutButton>
+      <LogoutButton onClick={handleLogout}>Logout</LogoutButton>
     </div>
   );
 };
