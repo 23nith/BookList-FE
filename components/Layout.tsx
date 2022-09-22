226e1926100c53c6a083f88dd542163c6339c73d
23nith <zenithmanuel@gmail.com>
2022-09-22 14:17:00 +0800

    Add select-none

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,7 +9,7 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="flex flex-col h-100vh">
+    <div className="flex flex-col h-100vh select-none">
       <Menubar />
       <div className="flex justify-center grow">
         <div className="max-w-840 flex flex-row h-full w-full">
